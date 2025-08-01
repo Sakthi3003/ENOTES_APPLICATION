96cf16729df38c3cc998fb19247f86346dfe1844
Sakthi3003 <tsakthisowmya30@gmail.com>
2025-08-01 18:05:35 +0530

    Added logging with log4j backend and slf

--- ENOTES/Enotes/src/main/java/com/enotes/Enotes/EnotesApplication.java
+++ ENOTES/Enotes/src/main/java/com/enotes/Enotes/EnotesApplication.java
@@ -1,15 +1,23 @@
 package com.enotes.Enotes;
 
+
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.apache.logging.log4j.LogManager;
+
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAware")
 public class EnotesApplication {
-
+	//private static final Logger logger = LoggerFactory.getLogger(EnotesApplication.class);
+	private static final Logger logger = LogManager.getLogger(EnotesApplication.class);
 	public static void main(String[] args) {
+
 		SpringApplication.run(EnotesApplication.class, args);
+		logger.info("hi");
+		logger.debug("hello");
 	}
 
 }
